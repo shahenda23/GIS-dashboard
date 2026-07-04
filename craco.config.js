9d8debeff4e96ca6f462e7ff448a254f69b10cd6
shahenda23 <shahendakhaled267@gmail.com>
2026-07-04 22:12:29 +0300

    babel

--- craco.config.js
+++ craco.config.js
@@ -2,6 +2,15 @@ module.exports = {
   webpack: {
     configure: (webpackConfig) => {
       webpackConfig.optimization.minimize = false;
+
+      // Prevent source-map-loader from processing maplibre-gl (causes _toArray error)
+      webpackConfig.module.rules = webpackConfig.module.rules.map(rule => {
+        if (rule.enforce === 'pre') {
+          return { ...rule, exclude: /node_modules/ };
+        }
+        return rule;
+      });
+
       return webpackConfig;
     },
   },
